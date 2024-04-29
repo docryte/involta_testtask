b73549ddcd31fadb1d14dd65c555565079c9d6ee
docryte <docryte@gmail.com>
2024-04-29 23:04:54 +0300

    Часть CRUD

--- go.mod
+++ go.mod
@@ -5,14 +5,45 @@ go 1.22.2
 require github.com/restream/reindexer/v3 v3.24.0
 
 require (
+	github.com/bytedance/sonic v1.9.1 // indirect
+	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
+	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator/v10 v10.14.0 // indirect
+	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
+	github.com/labstack/gommon v0.4.2 // indirect
+	github.com/leodido/go-urn v1.2.4 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
+	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
+	github.com/ugorji/go/codec v1.2.11 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
+	golang.org/x/arch v0.3.0 // indirect
+	golang.org/x/crypto v0.22.0 // indirect
+	golang.org/x/net v0.24.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
+
+require (
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/gin-gonic/gin v1.9.1
 	github.com/go-logr/logr v1.2.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/hashicorp/golang-lru v0.6.0 // indirect
 	github.com/iancoleman/orderedmap v0.0.0-20190318233801-ac98e3ecb4b0 // indirect
+	github.com/labstack/echo/v4 v4.12.0
 	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
 	github.com/prometheus/client_golang v1.12.2 // indirect
 	github.com/prometheus/client_model v0.2.0 // indirect
@@ -20,7 +51,7 @@ require (
 	github.com/prometheus/procfs v0.7.3 // indirect
 	go.opentelemetry.io/otel v1.14.0 // indirect
 	go.opentelemetry.io/otel/trace v1.14.0 // indirect
-	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
-	google.golang.org/protobuf v1.26.0 // indirect
+	golang.org/x/sys v0.19.0 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,42 +3,53 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
+	"time"
 
 	// Reindexer
 	"github.com/restream/reindexer/v3"
 	_ "github.com/restream/reindexer/v3/bindings/cproto"
+
+	// Echo для API
+	"github.com/labstack/echo/v4"
 )
 
 type Achievement struct {
-	Content	string	`reindex:"content"`
-	Date	int64	`reindex:"date"`
+	Content	string		`reindex:"content" json:"content" binding:"required"`
+	Date	time.Time	`reindex:"date" json:"date"`
 }
 
 type Job struct {
-	StartedAt		int64			`reindex:"started_at"`
-	EndedAt			int64			`reindex:"ended_at"`
-	Name 			string			`reindex:"name"`
-	Type 			string			`reindex:"type"`
-	Position 		string 			`reindex:"position"`
-	DismissalReason	string			`reindex:"dissmisal_reason"`
-	Achievements	[]Achievement	`reindex:"achievements"`
+	StartedAt		time.Time		`reindex:"started_at" json:"started_at" binding:"required"`
+	EndedAt			time.Time		`reindex:"ended_at" json:"ended_at" binding:"gtfield=StartedAt"`
+	Name 			string			`reindex:"name" json:"name" binding:"required"`
+	Type 			string			`reindex:"type" json:"type" binding:"required"`
+	Position 		string 			`reindex:"position" json:"position" binding:"required"`
+	DismissalReason	string			`reindex:"dissmisal_reason" json:"dismissal_reason"`
+	Achievements	[]Achievement	`reindex:"achievements" json:"achievements"`
+}
+
+type PersonPost struct {
+	FirstName	string		`reindex:"first_name" json:"first_name" binding:"required"`
+	LastName	string 		`reindex:"last_name" json:"last_name" binding:"required"`
+	Username	string		`reindex:"username,hash" json:"username" binding:"required"`
+	Birthdate	time.Time	`reindex:"birthdate" json:"birthdate" binding:"required"`
+	Profession	string		`reindex:"profession" json:"profession" binding:"required"`
+	Jobs		[]Job 		`reindex:"jobs" json:"jobs" binding:"required"`
 }
 
 type Person struct {
-	ID 			int64 		`reindex:"id,,pk"`
-	FirstName	string		`reindex:"first_name"`
-	SecondName	string 		`reindex:"second_name"`
-	Username	string		`reindex:"username,hash"`
-	Birthdate	int64		`reindex:"birthdate"`
-	Profession	string		`reindex:"profession"`
-	Jobs		[]Job 		`reindex:"jobs"`
-	CreatedAt 	int64		`reindex:"created_at"`
-	UpdatedAt	int64		`reindex:"updated_at"`
+	ID 			int64 		`reindex:"id,,pk" json:"_id"`
+	PersonPost
+	CreatedAt 	time.Time	`reindex:"created_at" json:"created_at"`
+	UpdatedAt	time.Time	`reindex:"updated_at" json:"updated_at"`
 }
+
+var db *reindexer.Reindexer
 
 func main() {
 	// Подключение к Reindexer по ссылке из окружения и проверка подключения
-	db := reindexer.NewReindex(os.Getenv("RX_CONNECTION_URL"), reindexer.WithCreateDBIfMissing())
+	db = reindexer.NewReindex(os.Getenv("RX_CONNECTION_URL"), reindexer.WithCreateDBIfMissing())
 	if err := db.Status().Err; err != nil {
 		fmt.Println("Error connecting to Reindex: ", err)
 		os.Exit(-1)
@@ -48,4 +59,65 @@ func main() {
 	if err != nil {
 		fmt.Println("Error creating namespace \"persons\": ", err)
 	}
+	// Создание и запуск роутера для API и CRUD
+	router := echo.New()
+	router.GET("/persons", getPersons)
+	router.POST("/persons", postPerson)
+	router.GET("/persons/:id", getPerson)
+	router.PATCH("/persons/:id", updatePerson)
+	router.DELETE("/persons/:id", deletePerson)
+	router.Logger.Fatal(router.Start(os.Getenv("API_ADDR")))
+}
+
+func getPersons(c echo.Context) error {
+	p := 0; l := 10
+	echo.QueryParamsBinder(c).Int("page", &p).Int("limit", &l)
+	result, found := db.Query("persons").Limit(l).Offset(p*l).Get()
+	if !found {
+		return c.String(404, "Not Found")
+	}
+	return c.JSON(200, result)
+}
+
+func postPerson(c echo.Context) error {
+	return c.String(500, "Doesn't Work")
+	/*p := new(PersonPost)
+	if err := c.Bind(&p); err != nil { return err }
+	person := Person{
+		0,
+		*p,
+		time.Now().UTC(),
+		time.Now().UTC(),
+	}
+	if _, err := db.Insert("persons", person); err != nil {
+		return c.String(500, fmt.Sprint("Error creating new person: ", err))
+	}
+	return c.JSON(200, person)*/
+}
+
+func getPerson(c echo.Context) error {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return c.String(400, fmt.Sprint("Bad ID: ", id))
+	}
+	result, found := db.Query("persons").Where("_id", reindexer.EQ, id).Get()
+	if !found {
+		return c.String(404, "Not Found")
+	}
+	return c.JSON(200, result) 
+}
+
+func updatePerson(c echo.Context) error {
+	return c.String(500, "Doesn't Work")
+}
+
+func deletePerson(c echo.Context) error {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return c.String(400, fmt.Sprint("Bad ID: ", id))
+	}
+	if _, err := db.Query("persons").Where("_id", reindexer.EQ, id).Delete(); err != nil {
+		return c.String(500, fmt.Sprint("Error deleting document by ID: ", err.Error()))
+	}
+	return c.String(200, "Success")
 }
