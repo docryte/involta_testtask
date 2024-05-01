f464b876841560c529bb2262369de9d692527ead
docryte <docryte@gmail.com>
2024-05-01 14:43:42 +0300

    Добавлено кэширование GET единственного документа через Redis, и чуточку 🤏 больше комментариев

--- go.mod
+++ go.mod
@@ -5,26 +5,33 @@ go 1.22.2
 require github.com/restream/reindexer/v3 v3.24.0
 
 require (
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
+	github.com/vmihailenco/go-tinylfu v0.2.2 // indirect
+	github.com/vmihailenco/msgpack/v5 v5.3.4 // indirect
+	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
 	golang.org/x/crypto v0.22.0 // indirect
 	golang.org/x/net v0.24.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 )
 
 require (
 	github.com/beorn7/perks v1.0.1 // indirect
-	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/go-logr/logr v1.2.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-playground/validator v9.31.0+incompatible
+	github.com/go-redis/cache/v9 v9.0.0
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/hashicorp/golang-lru v0.6.0 // indirect
@@ -35,6 +42,7 @@ require (
 	github.com/prometheus/client_model v0.2.0 // indirect
 	github.com/prometheus/common v0.32.1 // indirect
 	github.com/prometheus/procfs v0.7.3 // indirect
+	github.com/redis/go-redis/v9 v9.5.1
 	go.opentelemetry.io/otel v1.14.0 // indirect
 	go.opentelemetry.io/otel/trace v1.14.0 // indirect
 	golang.org/x/sys v0.19.0 // indirect
--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
+	"strconv"
 	"time"
 
 	// Reindexer
@@ -14,6 +16,10 @@ import (
 
 	// Echo для API
 	"github.com/labstack/echo/v4"
+
+	// Redis для кэша
+	"github.com/redis/go-redis/v9"
+	"github.com/go-redis/cache/v9"
 )
 
 type Achievement struct {
@@ -40,13 +46,27 @@ type PersonPost struct {
 	Jobs		[]Job 		`reindex:"jobs" json:"jobs" validate:"required"`
 }
 
+/*
+Структура документа с двойной вложенностью, как в задании
+
+Разделение на две структуры, потому что я не приудмал, 
+как лучше гарантировать, что пользователь в теле json не передаст _id или updated_at
+
+И в доках echo есть что-то похожее:
+https://echo.labstack.com/docs/binding#security
+*/
 type Person struct {
 	ID 			int64 		`reindex:"id,,pk" json:"_id"`
 	PersonPost
 	UpdatedAt	time.Time	`reindex:"updated_at" json:"updated_at"`
 }
 
-// Структуры для валидации запросов с json
+/* 
+Структуры для валидации запросов с json
+
+В доках echo:
+https://echo.labstack.com/docs/request#validate-data
+*/
 type PlaygroundValidator struct {
 	validator *validator.Validate
 }
@@ -58,21 +78,42 @@ func (pv *PlaygroundValidator) Validate(i interface{}) error {
 	return nil
 }
 
-var db *reindexer.Reindexer
+var reindexer_db *reindexer.Reindexer
+var redis_cache *cache.Cache
 
 func main() {
 	// Подключение к Reindexer по ссылке из окружения и проверка подключения
-	db = reindexer.NewReindex(os.Getenv("RX_CONNECTION_URL"), reindexer.WithCreateDBIfMissing())
-	if err := db.Status().Err; err != nil {
+	reindexer_db = reindexer.NewReindex(os.Getenv("REINDEXER_URL"), reindexer.WithCreateDBIfMissing())
+	if err := reindexer_db.Status().Err; err != nil {
 		fmt.Println("Error connecting to Reindex: ", err)
 		os.Exit(-1)
 	}
 	// Проверка и создание коллекции persons, в случае, если она отсутствует
-	err := db.OpenNamespace("persons", reindexer.DefaultNamespaceOptions(), Person{})
+	err := reindexer_db.OpenNamespace(
+		"persons", 
+		reindexer.DefaultNamespaceOptions(), 
+		Person{},
+	)
 	if err != nil {
 		fmt.Println("Error creating namespace \"persons\": ", err)
 		os.Exit(-1)
 	}
+
+	// Подключение к Redis и создание клиента для кэша
+	database, err := strconv.Atoi(os.Getenv("REDIS_DATABASE"))
+	if err != nil {
+		fmt.Printf("REDIS_DATABASE environment variable should be int, not: %v", os.Getenv("REDIS_DATABASE"))
+		os.Exit(-1)
+	}
+	redis_db := redis.NewClient(&redis.Options{
+        Addr:     os.Getenv("REDIS_URL"),
+        Password: os.Getenv("REDIS_PASSWORD"),
+        DB:       database,
+    })
+	redis_cache = cache.New(&cache.Options{
+		Redis: redis_db,
+	})
+
 	// Создание и запуск роутера для API и CRUD
 	router := echo.New()
 	router.Validator = &PlaygroundValidator{validator: validator.New()}
@@ -81,7 +122,7 @@ func main() {
 	router.GET("/persons/:id", getPerson)
 	router.PUT("/persons/:id", putPerson)
 	router.DELETE("/persons/:id", deletePerson)
-	router.Logger.Fatal(router.Start(os.Getenv("API_ADDR")))
+	router.Logger.Fatal(router.Start(os.Getenv("API_URL")))
 }
 
 func getPersons(c echo.Context) error {
@@ -89,7 +130,7 @@ func getPersons(c echo.Context) error {
 	if err := echo.QueryParamsBinder(c).Int("page", &p).Int("limit", &l).BindError(); err != nil {
 		return echo.NewHTTPError(400, err.Error())
 	}
-	result, err := db.Query("persons").Limit(l).Offset(p*l).Exec().FetchAll()
+	result, err := reindexer_db.Query("persons").Limit(l).Offset(p*l).Exec().FetchAll()
 	if err != nil {
 		return echo.NewHTTPError(500, fmt.Sprint("Error retrieving Person: ", err))
 	}
@@ -109,7 +150,7 @@ func postPerson(c echo.Context) error {
 		*p,
 		time.Now().UTC(),
 	}
-	_, err := db.Insert(
+	_, err := reindexer_db.Insert(
 		"persons",
 		&person,
 		"ID=serial()",
@@ -125,11 +166,30 @@ func getPerson(c echo.Context) error {
 	if err := echo.PathParamsBinder(c).Int64("id", &id).BindError(); err != nil {
 		return echo.NewHTTPError(400, err.Error())
 	}
-	result, found := db.Query("persons").Where("_id", reindexer.EQ, id).Get()
-	if !found {
-		return c.String(404, "Not Found")
+	person := new(Person)
+	// Сначала пробуем получить документ из кэша
+	err := redis_cache.Get(context.Background(), string(rune(id)), person)
+	/* 
+	Если документа нет в кэше, метод вернёт ошибку ErrCacheMiss
+	Ошибку обрабатываем для получения документа из БД и его записи в кэш
+	
+	Возможно, имеет смысл сначала вызывать Exists для проверки кэша на наличие
+	*/
+	if err == cache.ErrCacheMiss {
+		result, found := reindexer_db.Query("persons").Where("_id", reindexer.EQ, id).Get()
+		if !found {
+			return echo.NewHTTPError(404, "Not Found")
+		}
+		person = result.(*Person)
+		redis_cache.Set(&cache.Item{
+			Ctx: 	context.Background(),
+			Key: 	string(rune(id)),
+			Value: 	person,
+			// Объект в кэше хранится ровно 15 минут	
+			TTL:	time.Duration(time.Duration.Minutes(15)),
+		})
 	}
-	return c.JSON(200, result) 
+	return c.JSON(200, person) 
 }
 
 func putPerson(c echo.Context) error {
@@ -146,7 +206,7 @@ func putPerson(c echo.Context) error {
 		*p,
 		time.Now().UTC(),
 	}
-	_, err := db.Update(
+	_, err := reindexer_db.Update(
 		"persons",
 		&person,
 	)
@@ -161,7 +221,7 @@ func deletePerson(c echo.Context) error {
 	if err := echo.PathParamsBinder(c).Int64("id", &id).BindError(); err != nil {
 		return echo.NewHTTPError(400, err.Error())
 	}
-	if err := db.Delete("persons", &Person{ID: int64(id)}); err != nil {
+	if err := reindexer_db.Delete("persons", &Person{ID: int64(id)}); err != nil {
 		return c.String(500, fmt.Sprint("Error deleting document by ID: ", err.Error()))
 	}
 	return c.String(200, "Success")
